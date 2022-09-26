092909e7bd0cc25a32a5172fc5744f28150eb3cc
homuramadoka <homu@ramadoka.com>
2022-09-26 02:47:36 +0000

    chore: keepConnect but cannot die

--- main.go
+++ main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"github.com/SiestaMadokaist/tunnel-vision-go/service/ClientHub"
+)
+
+func main() {
+	// signalChan := make(chan os.Signal, 1)
+	// wg := sync.WaitGroup{};
+	// wg.Add(1);
+	// signal.Notify(signalChan, os.Interrupt)
+	// go func() {
+	// 	defer wg.Done();
+	// 	<- signalChan
+		
+	// }();
+
+	requestQueue := "https://sqs.ap-southeast-1.amazonaws.com/674152176016/ramadoka-request-1"
+	responseQueue := "https://sqs.ap-southeast-1.amazonaws.com/674152176016/ramadoka-response-1"
+
+	incoming := ClientHub.IncomingFields{ QueueURL: requestQueue, Hostname:  "incoming-host" };
+	outgoing := ClientHub.OutgoingFields{ QueueURL: responseQueue, Hostname: "outgoing-host" };
+	hub := ClientHub.New(&incoming, &outgoing);
+	hub.Start()
+}
--- service/ClientHub/ClientHub.go
+++ service/ClientHub/ClientHub.go
@@ -1,23 +1,66 @@
 package ClientHub
 
 import (
+	"fmt"
+	"time"
+
 	consumer "github.com/academy-software/go-aws-sqs-consumer"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
 )
 
-type ClientHub struct {
-	consumer *consumer.Consumer
+type IncomingFields struct {
+	QueueURL string;
+	Hostname string;
 }
 
-func New(queueURL string) ClientHub {
+type OutgoingFields struct {
+	QueueURL string;
+	Hostname string;
+}
+
+type ClientHub struct {
+	incoming *IncomingFields;
+	outgoing *OutgoingFields;
+	requestConsumer *consumer.Consumer;
+}
+
+
+func New(incoming *IncomingFields, outgoing *OutgoingFields) ClientHub {
 	sess := session.Must(session.NewSessionWithOptions(session.Options{ SharedConfigState: session.SharedConfigEnable }));
 	config := consumer.Config{ AwsSession: sess, Receivers: 1, SqsMaxNumberOfMessages: 10, SqsMessageVisibilityTimeout: 20, PollDelayInMilliseconds: 100}
-	c := consumer.New(queueURL, handle, &config)
-	return ClientHub{ consumer: &c }
+	requestConsumer := consumer.New(incoming.QueueURL, handle, &config);
+	hub := ClientHub{ incoming: incoming, outgoing: outgoing, requestConsumer: &requestConsumer };
+	return hub;
+}
+
+func (hub *ClientHub) connect(i int) {
+	fmt.Println(i);
+	fmt.Println(hub.incoming.QueueURL);
+	fmt.Println(hub.incoming.Hostname);
+	fmt.Println(hub.outgoing.QueueURL);
+	fmt.Println(hub.outgoing.Hostname);
+}
+
+func (hub *ClientHub) keepConnect(c chan string) {
+	for i := 0; i < 1; i+=0 {
+		time.Sleep(time.Second * 1);
+		hub.connect(i)
+	}
+	c <- "ok"
+}
+
+func (hub *ClientHub) Start() {
+	// var ok string;
+	c := make(chan string, 1)
+	// go hub.requestConsumer.Start();
+	go hub.keepConnect(c);
+	ok := <- c;
+	fmt.Println(ok);
 }
 
 func handle(m *sqs.Message) error {
+	fmt.Println(m.Body);
 	return nil;
 }
 
